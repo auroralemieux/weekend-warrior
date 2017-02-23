ea896250146f0fd8401c6145902023431f46ac35
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-02-23 07:21:15 -0800

    added array of lines for verse

--- 02-alouette/alouette.rb
+++ 02-alouette/alouette.rb
@@ -3,7 +3,18 @@ class Alouette
   def initialize
   end
 
-  def lines_for_verse
+  def lines_for_verse(how_many_lines)
+    lines = how_many_lines + 1
+    all_lines = [
+      "Et la tête!",
+      "Et le bec!",
+      "Et les yeux!",
+      "Et le cou!",
+      "Et les ailes!",
+      "Et les pattes!",
+      "Et la queue!",
+      "Et le dos!"
+    ]
   end
 
   def verse
@@ -11,5 +22,5 @@ class Alouette
 
   def sing
   end
-  
+
 end
