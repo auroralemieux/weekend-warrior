1003211f0f7b856458acfcb9fdfdd548853fc82c
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-02-23 09:17:27 -0800

    self.lines for verse tests passing

--- 02-alouette/alouette.rb
+++ 02-alouette/alouette.rb
@@ -1,10 +1,10 @@
 class Alouette
-
+  # attr_reader :verse_lines
   def initialize
+
   end
 
-  def lines_for_verse(how_many_lines)
-    lines = how_many_lines + 1
+  def self.lines_for_verse(how_many_lines)
     all_lines = [
       "Et la tête!",
       "Et le bec!",
@@ -15,12 +15,19 @@ class Alouette
       "Et la queue!",
       "Et le dos!"
     ]
+    verse_lines = []
+    all_lines[0..how_many_lines].each do |song_line|
+      verse_lines << song_line
+    end
+    verse_lines.reverse!
+    return verse_lines
   end
 
-  def verse
+  def self.verse(how_many_verses)
+
   end
 
-  def sing
+  def self.sing
   end
 
 end
